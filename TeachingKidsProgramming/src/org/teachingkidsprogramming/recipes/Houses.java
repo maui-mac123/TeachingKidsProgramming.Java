c1819ed674fd50f6c0e51e4c8d469f1eae2e77b6
Mauricio Macias <Mauricio.macias>
2013-06-19 15:06:18 -0700

    houses-Mauricio&Louis

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/Houses.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/Houses.java
@@ -1,26 +1,72 @@
 package org.teachingkidsprogramming.recipes;
 
+import org.teachingextensions.logo.Colors;
+import org.teachingextensions.logo.Tortoise;
+
 public class Houses
 {
+  private static int height = 40;
+  private static int width  = 10;
   public static void main(String[] args)
   {
     //   Make the tortoise move as fast as possible --#11
+    Tortoise.setSpeed(10);
     //   Have the tortoise start at 200 pixels in on the X axis --#14
-    //   The current height is 40 --#1.2
+    Tortoise.setX(200);
+    drawHouse(40);
+    //   ------------- End of drawHouse recipe
+    //   DrawHouse with height 120 (recipe below) --#10
+    drawHouse(120);
+    //   DrawHouse with height 90 (recipe below) --#12
+    drawHouse(90);
+    //   DrawHouse with height 20 (recipe below) --#13
+    drawHouse(20);
+  }
+  private static void drawHouse()
+  {
     //   drawHouse (recipe below) --#9
     //   ------------- Recipe for drawHouse --#9
     //   Change the color of the line the tortoise draws to lightGray --#15
-    //   Move the tortoise the height of a house --#1.1
+    Tortoise.move(height);
     //   Turn the tortoise 90 degrees to the right --#2
+    Tortoise.turn(90);
     //   Move the tortoise 30 pixels --#3
+    Tortoise.move(width);
     //   Turn the tortoise 90 degrees to the right --#4
+    Tortoise.turn(90);
     //   Move the tortoise the height of a house --#5
+    Tortoise.move(height);
     //   Turn the tortoise 90 degrees to the left --#6
+    Tortoise.turn(270);
     //   Move the tortoise 20 pixels --#7
+    Tortoise.move(20);
     //   Turn the tortoise 90 degrees to the left --#8
-    //   ------------- End of drawHouse recipe
-    //   DrawHouse with height 120 (recipe below) --#10
-    //   DrawHouse with height 90 (recipe below) --#12
-    //   DrawHouse with height 20 (recipe below) --#13
+    Tortoise.turn(270);
+    height += 40;
+    //height *= -1;
+    width += 20;
+    drawHouse();
+  }
+  private static void drawHouse(int height)
+  {
+    //   drawHouse (recipe below) --#9
+    //   ------------- Recipe for drawHouse --#9
+    //   Change the color of the line the tortoise draws to lightGray --#15
+    Tortoise.setPenColor(Colors.Grays.LightGray);
+    Tortoise.move(height);
+    //   Turn the tortoise 90 degrees to the right --#2
+    Tortoise.turn(90);
+    //   Move the tortoise 30 pixels --#3
+    Tortoise.move(30);
+    //   Turn the tortoise 90 degrees to the right --#4
+    Tortoise.turn(90);
+    //   Move the tortoise the height of a house --#5
+    Tortoise.move(height);
+    //   Turn the tortoise 90 degrees to the left --#6
+    Tortoise.turn(270);
+    //   Move the tortoise 20 pixels --#7
+    Tortoise.move(20);
+    //   Turn the tortoise 90 degrees to the left --#8
+    Tortoise.turn(270);
   }
 }
--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
@@ -1,22 +1,89 @@
 package org.teachingkidsprogramming.recipes;
 
-import org.teachingextensions.logo.Colors;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+
 import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.Turtle.Animals;
 import org.teachingextensions.windows.MessageBox;
 
-public class SimpleSquare
+public class SimpleSquare extends JFrame implements KeyListener, ActionListener
 {
   public static void main(String[] args) throws Exception
   {
+    Tortoise.getBackgroundWindow().setBounds(
+        new Rectangle(0, 0, Toolkit.getDefaultToolkit().getScreenSize().width, Toolkit.getDefaultToolkit()
+            .getScreenSize().height));
+    Tortoise.setAnimal(Animals.Spider);
     Tortoise.show();
+    //  Make the tortoise move as fast as possible --#6
     Tortoise.setSpeed(10);
-    int sides = MessageBox.askForNumericalInput("How many sides would you like?");
-    for (int i = 1; i <= sides; i++)
+    Tortoise.turn(MessageBox.askForNumericalInput("Initial Rotation"));
+    Tortoise.setX(MessageBox.askForNumericalInput("X Pos"));
+    Tortoise.setY(MessageBox.askForNumericalInput("Y Pos"));
+    new SimpleSquare().moveMe();
+  }
+  SimpleSquare()
+  {
+    super();
+  }
+  public void moveMe()
+  {
+    int penWidth = 5;
+    float hue = 0;
+    int angle = 15;
+    int move = 1;
+    while (true)
     {
-      Tortoise.setPenColor(Colors.getRandomColor());
-      Tortoise.setPenWidth(i * 3.5);
-      Tortoise.move(i * 5);
-      Tortoise.turn(-360.0 * 3 / sides);
+      Tortoise.setPenWidth(penWidth);
+      Tortoise.setPenColor(Color.getHSBColor(hue, 1, 1));
+      Tortoise.move(move);
+      Tortoise.turn(angle);
+      //penWidth++;
+      hue += .01;
+      if (hue > 1)
+      {
+        hue = 0;
+      }
+      angle += 1;
+      if (move < 20)
+        move++;
+      else
+        move--;
+      repaint();
     }
+  }
+  public void paint(Graphics g)
+  {
+    g.setColor(Color.BLACK);
+    g.drawString("testas;dlfja;sldkfj", 100, 100);
+  }
+  @Override
+  public void keyPressed(KeyEvent e)
+  {
+    // TODO Auto-generated method stub
+  }
+  @Override
+  public void keyReleased(KeyEvent e)
+  {
+    // TODO Auto-generated method stub
+  }
+  @Override
+  public void keyTyped(KeyEvent e)
+  {
+    // TODO Auto-generated method stub
+  }
+  @Override
+  public void actionPerformed(ActionEvent e)
+  {
+    // TODO Auto-generated method stub
   }
 }
