fa92c3fd2651aa78e152b3e26bd79f11c0d6c0d7
Mauricio Macias <Mauricio.macias123@gmail.com>
2013-06-25 15:30:29 -0700

    Turtle Tree Circle Mauricio Aaron

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/TurtleTree.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/TurtleTree.java
@@ -8,10 +8,15 @@ public class TurtleTree
   public static void main(String[] args)
   {
     Turtle turtle = new Turtle();
-    turtle.setSpeed(8);
+    turtle.setSpeed(10);
     turtle.getBackgroundWindow().setBackground(Colors.Grays.Black);
     int branchlength = 60;
-    drawBranch(turtle, branchlength);
+    int turningamount = 10;
+    for (int i = 1; i <= 360 / turningamount; i++)
+    {
+      drawBranch(turtle, branchlength);
+      turtle.turn(turningamount);
+    }
   }
   public static void drawBranch(Turtle turtle, int branchlength)
   {
@@ -30,23 +35,23 @@ public class TurtleTree
     }
     if (branchlength == 20)
     {
-      turtle.setPenColor(Colors.Greens.ForestGreen);
+      turtle.setPenColor(Colors.Whites.White);
     }
     if (branchlength == 30)
     {
-      turtle.setPenColor(Colors.Greens.DarkGreen);
+      turtle.setPenColor(Colors.Greens.MediumSpringGreen);
     }
     if (branchlength == 40)
     {
-      turtle.setPenColor(Colors.Greens.Olive);
+      turtle.setPenColor(Colors.Greens.Green);
     }
     if (branchlength == 50)
     {
-      turtle.setPenColor(Colors.Browns.Sienna);
+      turtle.setPenColor(Colors.Blues.CornflowerBlue);
     }
     if (branchlength == 60)
     {
-      turtle.setPenColor(Colors.Browns.SaddleBrown);
+      turtle.setPenColor(Colors.Yellows.Gold);
     }
   }
   public static void drawLowerBranches(Turtle turtle, int branchlength)
